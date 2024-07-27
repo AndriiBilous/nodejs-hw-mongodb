71c8906a9a2e065abdb29cf0d39ce400752f7578
Andrii <andriybilous121392@gmail.com>
2024-07-27 15:09:33 -0500

    to check

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -6,13 +6,27 @@ import {
     updateContact,
     deleteContact,
 } from '../services/contacts.js';
+import { parsePaginationParams } from '../utils/parsePaginationParams.js';
+import { parseSortParams } from '../utils/parseSortParams.js';
+import { parseFilterParams } from '../utils/parseFilterParams.js';
 
 export const getContactsController = async (req, res, next) => {
-    const contacts = await getAllContacts();
-    res.status(200).json({
+    const { page, perPage } = parsePaginationParams(req.query);
+    const { sortBy, sortOrder } = parseSortParams(req.query);
+    const filter = parseFilterParams(req.query);
+
+    const contact = await getAllContacts({
+        page,
+        perPage,
+        sortBy,
+        sortOrder,
+        filter,
+    });
+
+    res.json({
         status: 200,
         message: 'Successfully found contacts!',
-        data: contacts,
+        data: contact,
     });
 };
 export const getContactsByIdController = async (req, res, next) => {
